0483428ebe9424c1eb4904d33fa0272e8648d4c7
Dino Pierro <dino.pierro@gmail.com>
2023-03-28 20:45:15 +0200

    Update Program.cs
    Integrazione swagger OpenAPI

--- Istat.UnitaTerritoriali/Program.cs
+++ Istat.UnitaTerritoriali/Program.cs
@@ -2,27 +2,14 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUnitaTerritorialiService, UnitaTerritorialiService>();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
-
